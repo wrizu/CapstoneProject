6d59caa2e284bce5ac0deb7f875262c6c2b734f3
wrizu <60866094+wrizu@users.noreply.github.com>
2025-04-28 16:39:11 -0700

    Delete utils directory

--- utils/xata.ts
+++
@@ -1,9 +0,0 @@
-import { XataClient } from '@xata.io/client';
-
-export const xata = new XataClient({
-  apiKey: process.env.XATA_API_KEY
-});
-
-// Set the database URL separately if needed
-xata.setDatabaseURL(process.env.DATABASE_URL!);
-
