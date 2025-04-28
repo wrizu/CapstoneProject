3dbbe4bd8b7765d6506d5195a031bb74ec98c8e6
wrizu <60866094+wrizu@users.noreply.github.com>
2025-04-28 16:31:27 -0700

    Update xata.ts

--- utils/xata.ts
+++ utils/xata.ts
@@ -1,2 +1,9 @@
-import { xata } from './utils/xata'
-export const xata = new XataClient({ apiKey: process.env.XATA_API_KEY })
+import { XataClient } from '@xata.io/client';
+
+export const xata = new XataClient({
+  apiKey: process.env.XATA_API_KEY
+});
+
+// Set the database URL separately if needed
+xata.setDatabaseURL(process.env.DATABASE_URL!);
+
