c19489a27d103b14ea2c4020d78d3421fc8c286e
wrizu <60866094+wrizu@users.noreply.github.com>
2025-04-28 14:43:24 -0700

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -3,7 +3,7 @@ import { Client } from '@xata.io/client'
 const xata = new Client({
   apiKey: xau_Lt9h0yiw4vlawsDh6G2oTAEDPERQdH0I1
 ,
-  databaseURL: https://Jack-Burkhalter-s-workspace-v15me3.us-east-1.xata.sh/db/VALORANT,
+  databaseURL: 'https://Jack-Burkhalter-s-workspace-v15me3.us-east-1.xata.sh/db/VALORANT',
 })
 
 export default async function handler(req, res) {
