238e36ae7b8e54657a3cf37c92e7298129374197
wrizu <60866094+wrizu@users.noreply.github.com>
2025-04-30 02:49:47 +0000

    upload

--- server.cjs
+++ server.cjs
@@ -4,7 +4,7 @@ const path = require('path');
 const { Pool } = require('pg');
 
 // Load environment variables
-dotenv.config({ path: path.resolve(__dirname, 'process.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
